628846b344493d11934d2da264dec239286e87f9
Mateusz Chojnacki <mateuszchojnacki@Mateuszs-MacBook-Pro.local>
2018-12-19 23:29:59 +0100

    secound ex

--- homework4.R
+++ homework4.R
@@ -17,4 +17,70 @@ surveyforest<-survey$ximp
 diag(cor(survey[, -5], surveyforest[,-5]))
 
 #2. Napisz poprawn?? p??tl?? for(lub zastosuj inn?? iteracj??) z ??wiczenia 9, kt??ra usunie punkty oddalone z wybranej zmiennej w zbiorze danych.
+library(outliers)
+library(MASS)      
+library(survey)
+#Sex Wr.Hnd NW.Hnd W.Hnd    Fold Pulse    Clap Exer Smoke Height      M.I    Age
+y.in<-survey
+
+for (i in survey){
+  y.out<-rm.outlier(y.in, fill = TRUE, median = TRUE, opposite = FALSE)
+  y.out
+  plot(y.out,pch=16,col=i,cex=1.5,main =paste("ss",i))
+  boxplot(y.out,pch=16,col=i,cex=1.5)
+  y.in<-y.out
+}
+
+
+x[which(x %in% boxplot.stats(x)$out)]
+a <- survey$Age[which(survey$Age %in% boxplot.stats(survey$Age)$out)]
+y.out<-rm.outlier(survey.$(i), fill = TRUE, median = TRUE, opposite = FALSE)
+
+
+
+library(outliers)
+x<- c(1, 60, 2, 1, 4, 4, 1, 1, 6, -30, 70)
+
+x[which(x %in% boxplot.stats(x)$out)]
+plot(x,pch=16,col=5,cex=1.5)
+hist(x,pch=16,col=5,cex=1.5)
+boxplot(x,pch=16,col=5,cex=1.5)
+
+y<-rm.outlier(x, fill = FALSE, median = FALSE, opposite = FALSE)
+plot(y,pch=16,col=6,cex=1.5)
+boxplot(y,pch=16,col=6,cex=1.5)
+hist(y,pch=16,col=6,cex=1.5)
+
+y.in<-x
+for(i in 1:length(x))
+{
+  y.out<-rm.outlier(y.in, fill = FALSE, median = FALSE, opposite = FALSE)
+  y.out
+  plot(y.out,pch=16,col=i,cex=1.5,main =paste("ss",i))
+  boxplot(y.out,pch=16,col=i,cex=1.5)
+  y.in<-y.out
+}
+
+library(MASS)      
+library(survey)
+survey <- missForest(survey)
+surveyforest<-survey$ximp
+diag(cor(survey[, -5], surveyforest[,-5]))
+
+y.in<-survey
+
+
+
+
+for(i in 1:length(x))
+{
+  y.out<-rm.outlier(y.in, fill = TRUE, median = TRUE, opposite = FALSE)
+  y.out
+  plot(y.out,pch=16,col=i,cex=1.5,main =paste("ss",i))
+  boxplot(y.out,pch=16,col=i,cex=1.5)
+  y.in<-y.out
+}
+
+
+
 
